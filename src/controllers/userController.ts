a9fa0c4f9b71b7f8981138febedb2fecb1e4fd37
Gaurav Upadhyay <gauravupadhyay786.gu@gmail.com>
2024-08-23 18:37:25 +0530

    test changes

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,8 +4,8 @@ import userService from '../services/userService';
 class UserController {
     public async register(req: Request, res: Response): Promise<void> {
         try {
-            const item = await userService.register(req, res);
-            res.status(201).json(item);
+            const data = await userService.register(req, res);
+            res.status(201).json(data);
         } catch (error: any) {
             res.status(500).json({ message: error.message });
         }
